c1d94694ac5fbbfd0883ec3e29c4029095e9c6cc
Nicolas Judalet <nicolas.judalet@gmail.com>
2018-09-06 09:30:17 +0200

    10 - Set up the timer logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,40 +4,47 @@ import PlayPauseButton from '../PlayPauseButton/PlayPauseButton'
 import ProgressBar from '../ProgressBar/ProgressBar';
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props);
 
     this.state = {
       timerRunning: false,
       timeElapsed: 0,
-      totalTime: 10,
-      percentElapsed: 0,
+      totalTime: 3,
+      timerFinished: false,
     };
   }
   
   toggleTimer = (e) => {
     e.preventDefault();
     this.setState((previousState) => ({ timerRunning: !previousState.timerRunning }));
+    if (this.state.timerRunning || this.state.totalTime === this.state.timeElapsed) {
+      this.stopTimer();
+    }
+    else {
+      this.startTimer();
+    } 
   }
 
   startTimer(){
-    this.timer = setInterval(() => this.setState((prevState) => ({
-      timeElapsed: prevState.timeElapsed + 1,
-    })), 1000);
+    this.timer = setInterval(() => {
+      if (this.state.totalTime !== this.state.timeElapsed){
+        this.setState((prevState) => ({
+          timeElapsed: prevState.timeElapsed + 1,
+        }));
+      } else {
+        this.stopTimer();
+        this.setState({
+          timerRunning: false,
+          timerFinished: true,
+        });
+      }
+    }, 1000);
   }
     
   stopTimer(){
     clearInterval(this.timer);
-  }
-
-  componentWillUpdate(nextProps, nextState){
-    if (nextState.timerRunning) {
-      this.startTimer();
-    }
-    if (!nextState.timerRunning) {
-      this.stopTimer();
-    } 
   }
 
   render() {
@@ -46,10 +53,6 @@ class App extends Component {
         <div className='BackgroundDiv'>
           <p className="Page-title">
             Mode Live
-            <br/>
-            timerRunning : {this.state.timerRunning ? 'Running' : 'Not Running'}
-            <br/>
-            timeElapsed : {this.state.timeElapsed}
           </p>
           
           <h2 className="Exercice-section">Rythme</h2>
@@ -65,12 +68,12 @@ class App extends Component {
               strokeWidth="4" 
               strokeColor="#D3D3D3" 
               flexGrow="1"
-              percentElapsed={this.state.percentElapsed}
+              percentElapsed={this.state.timeElapsed / this.state.totalTime * 100}
             />
           </div>
 
           <p>
-            10 min
+            {this.state.totalTime / 60} min
           </p>
           
         </div> 
