ee01e882f0eef991c949d815b214b64884107e1a
Nicolas Judalet <nicolas.judalet@gmail.com>
2018-10-03 11:22:13 +0200

    20-make-app-full-screen

--- src/components/App/App.style.js
+++ src/components/App/App.style.js
@@ -4,6 +4,7 @@ export const StyledApp = styled.div`
   transition: background-color 2s ease;
   background-color: ${props => (props.exerciseRunning ? '#3A3F51' : 'white')};
   text-align: center;
+  height: 100%;
   
   > h1 {
     margin-top: 0px;
@@ -35,6 +36,8 @@ export const StyledApp = styled.div`
     padding-left: 20px;
     color: ${props => (props.exerciseRunning ? 'white' : 'black')};
     transition: color 2s ease;
+    padding-block-start: 1em;
+    margin-block-start: 0;
   }
 `;
 
